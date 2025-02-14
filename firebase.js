a56921ddd069691bebd2e4f155e92f6b29580802
Prabhjeet Singh <prabhjeetsingh1013@gmail.com>
2025-02-15 03:50:25 +0530

    Firebase Bugs and Errors Fixed

--- firebase.js
+++ firebase.js
@@ -1,13 +1,11 @@
 import admin from "firebase-admin";
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS);
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
-
+// Initialize Firebase only if not already initialized
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
-const db = admin.firestore();
-
-export { db };
+export default admin.firestore();
